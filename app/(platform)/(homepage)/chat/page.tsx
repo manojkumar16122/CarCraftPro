0b43c03a83e417cf7c32691a4538ecb73d74e359
Manojkumar V <115546459+manojkumar16122@users.noreply.github.com>
2024-01-12 20:49:26 +0530

    Update1 page.tsx

--- app/(platform)/(homepage)/chat/page.tsx
+++ app/(platform)/(homepage)/chat/page.tsx
@@ -31,7 +31,7 @@ const ChatPage = () => {
     <div className="fixed mt-10 w-full mb-1 p-4 bg-white border-t flex flex-col items-center justify-center">
       <iframe
         seamless
-        src="https://39d0a68b771224532f.gradio.live/"
+        src="https://6a6080f97bb41670c8.gradio.live/"
         // Manojkumar
         width={iframeWidth}
         height={iframeHeight}
